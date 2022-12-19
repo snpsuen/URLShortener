3a445576521a23bd769b1c8d777b158fb4631d56
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-12-19 21:04:10 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import pickle, random, string
+
+def randomstring(length):
+    return ''.join(
+        random.choices(
+            string.ascii_letters + string.digits,
+            k=length
+        )
+    )
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/shortenurl', methods=['GET', 'POST'])
+def shortenurl():
+    if request.method == 'POST':
+      absolutepath = request.form['url'])
+      shorten = randomstring(8)
+      
+      urlmapfile = open("urlmap.pkl", "wb")
+      urlmapfile.close()
+      urlmapfile = open("urlmap.pkl", "rb")
+      urlmapdict = pickle.load(urlmap)
+      
+      urlmapdict[shorten] = absolutepath
+      pickle.dump(urlmapdict, urlmapfile)
+      urlmapfile.close()
+      return render_template('result.html')
+ 
